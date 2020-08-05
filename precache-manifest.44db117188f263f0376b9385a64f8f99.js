893f5442b832f97890e64522140bff3ca779f636
andy-anu <8765anurag@gmail.com>
2020-08-05 22:57:12 +0530

    Updates

---
+++ precache-manifest.44db117188f263f0376b9385a64f8f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b24739a092589e44cce005c6cdb5b56",
+    "url": "/messenger_app/index.html"
+  },
+  {
+    "revision": "86e8b486e4023d11770f",
+    "url": "/messenger_app/static/css/main.acd2a6ba.chunk.css"
+  },
+  {
+    "revision": "fd26c824f966feb8da7c",
+    "url": "/messenger_app/static/js/2.40ebe8e4.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger_app/static/js/2.40ebe8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e8b486e4023d11770f",
+    "url": "/messenger_app/static/js/main.a8459407.chunk.js"
+  },
+  {
+    "revision": "e12f177d2191842b30ec",
+    "url": "/messenger_app/static/js/runtime-main.6e94053f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/messenger_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
